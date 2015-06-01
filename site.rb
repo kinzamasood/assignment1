3c40c9e4e6dd3a0f549caca406e7435bee49926d
Kinza Masood <kinza.masood@utah.edu>
2015-06-01 06:52:10 +0000

    Add config.ru

--- site.rb
+++ site.rb
@@ -1,6 +1,10 @@
 require 'sinatra'
-set :port, 4000
-set :bind, '0.0.0.0'
+# set :port, 4000
+# set :bind, '0.0.0.0'
+
+ENV['PORT'] ||= '4000'
+set :port, ENV['PORT']
+
 
 get '/italian' do
   "Ciao!"
